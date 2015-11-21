e7e9e2f8668b1f9e0171492bb622620f45a14a27
Luca Molteni <volothamp@gmail.com>
2015-11-21 18:37:56 +0100

    Refactor ugly multiple FilePath

--- src/Main.hs
+++ src/Main.hs
@@ -3,29 +3,30 @@
 module Main where
 
 import Turtle
-import Filesystem.Path
+import Filesystem.Path (addExtension)
+import Prelude hiding (FilePath)
 
 main :: IO ()
 main = do (source, target)
             <- options "find all raw + jpeg pairs and copy to target" args
           sh $ copyAll source target
 
-args :: Parser (Turtle.FilePath, Turtle.FilePath)
+args :: Parser (FilePath, FilePath)
 args = (,) <$> argPath "sourceDir" "The source directory where to find"
                 <*> argPath "targetDirectory" "The directory to put the files"
 
-copy :: Turtle.FilePath -> Turtle.FilePath -> IO ()
+copy :: FilePath -> FilePath -> IO ()
 copy fp destinationDir = cp fp (destinationDir </> filename fp)
 
-copyAll :: Filesystem.Path.FilePath -> Filesystem.Path.FilePath -> Shell ()
+copyAll :: FilePath -> FilePath -> Shell ()
 copyAll source target = do files <- allFiles source
                            liftIO $ copy files target
 
-allFiles :: Filesystem.Path.FilePath -> Shell Filesystem.Path.FilePath
+allFiles :: FilePath -> Shell FilePath
 allFiles source = convertedJpegFiles <|> rafFiles
                 where rafFiles = find (suffix "RAF") source
                       convertedJpegFiles = fmap toJpeg rafFiles
 
-toJpeg :: Turtle.FilePath -> Turtle.FilePath
+toJpeg :: FilePath -> FilePath
 toJpeg fp = addExtension baseName "JPG"
             where baseName = dropExtension fp
